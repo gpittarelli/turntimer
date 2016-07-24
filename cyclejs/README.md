3725768ceae00e7f41cd552cbc7a0766b94ee372
George Pittarelli <g@gjp.cc>
2016-07-23 19:25:27 -0700

    Initial commit of cyclejs

---
+++ cyclejs/README.md
@@ -0,0 +1,5 @@
+# turn timer: cylcejs
+
+The turn timer, implemented in cylcejs
+
+Uses webpack to bundle the client, and nodejs+express for the server
---
+++ cyclejs/webpack.config.js
@@ -0,0 +1,26 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: 'dist',
+    filename: 'client.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/index.html',
+      hash: true,
+      minify: {
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+      }
+    })
+  ]
+};
