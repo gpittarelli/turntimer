e34e4fba5e65f22958c907cc640429c585ca71ec
George Pittarelli <g@gjp.cc>
2016-07-23 23:06:05 -0700

    Setup history routing via cyclic-router

--- cyclejs/client/index.js
+++ cyclejs/client/index.js
@@ -1,21 +1,47 @@
+import xs from 'xstream';
 import {run} from '@cycle/xstream-run';
-import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+import {div, label, a, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+import {makeRouterDriver} from 'cyclic-router';
+import {createHistory} from 'history';
 
 function main(sources) {
-  const sinks = {
+  const match$ = sources.router.define({
+    '/': mainPage,
+    '/other': secondPage,
+  });
+
+  const page$ = match$.map(
+    ({path, value}) =>
+      value({...sources, router: sources.router.path(path)}));
+
+  return { DOM: page$.map(c => c.DOM).flatten() };
+}
+
+function mainPage(sources) {
+  return {
     DOM: sources.DOM.select('.field').events('input')
       .map(ev => ev.target.value)
       .startWith('')
-      .map(name =>
-        div([
-          label('Name:'),
-          input('.field', {attrs: {type: 'text'}}),
-          hr(),
-          h1('Hello ' + name),
-        ])
-      )
+      .map(name => div([
+        label('Name:'),
+        input('.field', {attrs: {type: 'text'}}),
+        hr(),
+        h1('Hello ' + name),
+        a({attrs: {href: '/other'}}, 'hi this is a link'),
+      ])),
   };
-  return sinks;
 }
 
-run(main, { DOM: makeDOMDriver('#app-container') });
+function secondPage() {
+  return {
+    DOM: xs.of(div([
+      h1('Other page'),
+      a({attrs: {href: '/'}}, 'Back home'),
+    ])),
+  };
+}
+
+run(main, {
+  DOM: makeDOMDriver('#app-container'),
+  router: makeRouterDriver(createHistory(), {capture: true}),
+});
--- cyclejs/webpack.config.js
+++ cyclejs/webpack.config.js
@@ -24,4 +24,8 @@ module.exports = {
       },
     }),
   ],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: 'dist',
+  },
 };
