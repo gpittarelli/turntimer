908e84427e26357661132442c4df04b57c4c691b
George Pittarelli <g@gjp.cc>
2016-07-26 01:09:28 -0700

    implement an MVI style

--- cyclejs/client/index.js
+++ cyclejs/client/index.js
@@ -1,57 +1,11 @@
 import xs from 'xstream';
 import {run} from '@cycle/xstream-run';
-import {div, h1, a, label, input, button, makeDOMDriver} from '@cycle/dom';
+import {div, a, h1, makeDOMDriver} from '@cycle/dom';
 import {makeRouterDriver} from 'cyclic-router';
 import {createHistory} from 'history';
 import {makeHTTPDriver} from '@cycle/http';
 
-function main(sources) {
-  const match$ = sources.router.define({
-    '/group/:id': id => sources => secondPage({...sources, id}),
-    '*': mainPage,
-  });
-
-  const page$ = match$.map(
-    ({path, value}) =>
-      value({...sources, router: sources.router.path(path)}));
-
-  return {
-    DOM: page$.map(c => c.DOM).flatten(),
-    HTTP: page$.map(c => c.HTTP || xs.never()).flatten(),
-    router: page$.map(c => c.router || xs.never()).flatten(),
-  };
-}
-
-function mainPage(sources) {
-  const data = xs.combine(
-    sources.DOM.select('.name').events('input')
-      .map(ev => ev.target.value)
-      .startWith(''),
-    sources.DOM.select('.group-id').events('input')
-      .map(ev => ev.target.value)
-      .startWith(0),
-    sources.DOM.select('.join-group').events('click').startWith(false)
-  );
-
-  return {
-    DOM: data.map(([name, groupId, click]) => div([
-      label('Name:'), input('.name', {attrs: {type: 'text'}}),
-      label('Group:'), input('.group-id', {attrs: {type: 'number'}}),
-      button('.join-group',
-             {attrs: {disabled: !(name && groupId) || click}},
-             'Join Group'),
-      a({attrs: {href: '/group'}}, 'go'),
-    ])),
-    HTTP: data.map(([name, groupId, click]) => click && {
-      method: 'POST',
-      url: `/api/group/${groupId}?name=${name}`,
-      category: 'join-group',
-    }),
-    router: sources.HTTP.select('join-group').flatten().map(
-      ({body: {id}}) => `/group/${id}`
-    ),
-  };
-}
+import Home from './home';
 
 function secondPage({id}) {
   return {
@@ -62,7 +16,20 @@ function secondPage({id}) {
   };
 }
 
-run(main, {
+function view({...sources, router}) {
+  const match$ = router.define({
+    '/group/:id': id => sources => secondPage({...sources, id}),
+    '*': Home,
+  }).map(({path, value}) => value({...sources, router: router.path(path)}));
+
+  return {
+    DOM: match$.map(c => c.DOM).flatten(),
+    HTTP: xs.of({url: '/asdf'}),
+    router: xs.never(),
+  };
+}
+
+run(view, {
   DOM: makeDOMDriver('#app-container'),
   router: makeRouterDriver(createHistory(), {capture: true}),
   HTTP: makeHTTPDriver(),
