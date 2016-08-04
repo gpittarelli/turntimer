a56eec4d1fd37fb010ef0d2e4b56749a57e54503
George Pittarelli <george.pittarelli@massdrop.com>
2016-08-03 23:23:35 -0700

    move to app-container; copy/paste hello world

--- preact-redux/client/index.js
+++ preact-redux/client/index.js
@@ -1,1 +1,14 @@
-document.write('Hello World');
+import { Provider, connect } from 'preact-redux';
+import { h, render } from 'preact';
+
+const Main = () => (
+  Provider({store, children: Child()})
+);
+
+const Child = connect(
+    state => state
+)( ({ text, setText }) => (
+  h('input', {value: text, onInput: e => setText(e.target.value)})
+));
+
+render(() => h('h1', {}, 'Hi1'), document.getElementById('app-container'));
