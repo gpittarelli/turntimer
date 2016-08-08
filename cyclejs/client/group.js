ae699e989364be19c582f975f992ea54f43b8c7e
George Pittarelli <george.pittarelli@massdrop.com>
2016-08-08 00:01:14 -0700

    Get user list and toggle-ready button working

--- cyclejs/client/group.js
+++ cyclejs/client/group.js
@@ -1,31 +1,74 @@
 import most from 'most';
-import {div, a, h1} from '@cycle/dom';
+import {div, a, h1, ul, li, button} from '@cycle/dom';
+import {nthArg} from 'ramda';
 import mavi from './mavi';
 import {toArray} from './helpers';
 
+const CHECK = '\u2713';
+
 const intent = () => ({});
 
-function model({player: player$, id}) {
+function model({player: player$, id, HTTP, DOM}) {
+  const group$ = HTTP.select('update-group').switch().map(({body}) => body),
+    playerName$ = player$.map(({name}) => name);
+
   return {
-    group$: most.of({id}),
-    player$: player$,
+    groupId$: most.of({id}),
+    group$,
+    joinState$: HTTP.select('join-group').switch().map(({body}) => body),
+    playerName$,
+    player$: most.combine(Array.of, group$, playerName$)
+      .map(([{users}, name]) => users.filter(u => u.name === name)[0]),
+    toggleReady$: DOM.select('.ready').events('click'),
   };
 }
 
-function view({group$, player$}) {
-  return most.combine(toArray, group$, player$).map(([{id}, {name}]) => div([
+function view({group$, playerName$, joinState$}) {
+  const joinStateUI$ = joinState$.map(
+    () => []
+  ).startWith([h1('Loading')]);
+
+  const userList$ = group$.map(({users}) => ul(users.map(
+    ({name, ready}) => li([name, ready ? CHECK : ''])
+  )));
+
+  return most.combineArray(Array.of, [
+    group$, userList$, playerName$,
+    joinStateUI$, joinState$.startWith('Loading'),
+  ]).map(([{...group, id}, userList, name, joinStateUI, joinState]) => div([
     a({attrs: {href: '/'}}, 'Back home'),
+    ...joinStateUI,
     h1(`Welcome to group ${id}, ${name}!`),
+    div(JSON.stringify(joinState)),
+    div(group ? JSON.stringify(group) : 'none'),
+    userList,
+    button('.ready', 'Ready'),
   ]));
 }
 
-function act({player$, group$}) {
+function act({playerName$, groupId$, toggleReady$, player$}) {
+  const state$ = most.combine(toArray, groupId$, playerName$);
+
   return {
-    HTTP: most.combine(toArray, group$, player$).map(([{id}, {name}]) => ({
-      method: 'post',
-      url: `/api/group/${id}/player/${name}`,
-      category: 'join-group',
-    })),
+    HTTP: most.merge(
+      most.combine(nthArg(0), groupId$, most.periodic(1000).startWith(0)).map(({id}) => ({
+        method: 'get',
+        url: `/api/group/${id}`,
+        category: 'update-group',
+      })),
+      state$.map(([{id}, name]) => ({
+        method: 'post',
+        url: `/api/group/${id}/player/${name}`,
+        category: 'join-group',
+      })),
+      toggleReady$.sample(toArray, state$, player$)
+        .map(([[{id}, name], {ready}]) => ({
+          method: 'patch',
+          url: `/api/group/${id}/player/${name}/update`,
+          send: {ready: !ready},
+          category: 'update-player',
+        }))
+    ),
     player: most.never(),
   };
 }
