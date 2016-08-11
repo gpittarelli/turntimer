f47c05bf0208040c17defec82f7211fee08f2d47
George Pittarelli <george.pittarelli@massdrop.com>
2016-08-11 01:02:20 -0700

    Responsive join group form

--- cyclejs/client/home.js
+++ cyclejs/client/home.js
@@ -1,8 +1,46 @@
 import most from 'most';
-import {div, a, label, input, button} from '@cycle/dom';
+import {form, div, label, input, button} from '@cycle/dom';
 import {prop, assoc, nthArg} from 'ramda';
 import mavi from './mavi';
 import {toArray, eventValue} from './helpers';
+import {StyleSheet, css} from 'aphrodite/no-important';
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: 'gray',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '5%',
+  },
+  formRow: {
+    margin: '10% 0 0',
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+  },
+  label: {
+    paddingRight: '10px',
+    fontSize: '20px',
+  },
+  input: {
+    flex: 1,
+    minWidth: '50px',
+    border: 0,
+    borderBottom: '2px black solid',
+    background: 'transparent',
+    fontSize: '20px',
+  },
+  numberInput: {
+    width: '5em',
+    flex: 'inherit',
+  },
+  joinButton: {
+    marginTop: '10%',
+    padding: '2em',
+    width: '50%',
+  },
+});
 
 function intent({DOM}) {
   const name$ = DOM.select('.name').events('input').map(eventValue),
@@ -31,10 +69,30 @@ function model({HTTP, player: player$, name$, groupId$, joinGroup$}) {
 }
 
 function view({joinButtonEnabled$}) {
-  return joinButtonEnabled$.map(enabled => div([
-    label('Name:'), input('.name', {attrs: {type: 'text'}}),
-    label('Group:'), input('.group-id', {attrs: {type: 'number'}}),
-    button('.join-group', {attrs: {disabled: enabled}}, 'Join Group'),
+  return joinButtonEnabled$.map(enabled => form({
+    class: {[css(styles.page)]: true},
+  }, [
+    div({class: {[css(styles.formRow)]: true}}, [
+      label({class:{[css(styles.label)]: true}}, 'Name:'),
+      input('.name', {
+        attrs: {required: true, type: 'text'},
+        class: {[css(styles.input)]: true},
+      }),
+    ]),
+    div({class: {[css(styles.formRow)]: true}}, [
+      label({class:{[css(styles.label)]: true}}, 'Group:'),
+      input('.group-id', {
+        attrs: {required: true, type: 'number'},
+        class: {
+          [css(styles.input)]: true,
+          [css(styles.numberInput)]: true,
+        },
+      }),
+    ]),
+    button('.join-group', {
+      attrs: {disabled: enabled},
+      class: {[css(styles.joinButton)]: true},
+    }, 'Join Group'),
   ]));
 }
 
