afe567e2d58726329dbf938d1c06caa12be850fe
George Pittarelli <george.pittarelli@massdrop.com>
2016-08-19 23:45:34 -0700

    curry some of the helper functions

--- cyclejs/lib/centerAround.js
+++ cyclejs/lib/centerAround.js
@@ -1,12 +1,15 @@
-// rotate by x to the left
-export const rotate = (x, arr) =>
-  arr.slice(x, arr.length).concat(arr.slice(0, x));
+import {curry} from 'ramda';
 
-export default function centerAround(x, arr) {
+// rotate by x to the left
+export const rotate = curry(
+  (x, arr) => arr.slice(x, arr.length).concat(arr.slice(0, x))
+);
+
+export default curry(function centerAround(x, arr) {
   // return the list arr (of length n), rotated such that the value
   // currently at index x is in the center (if n is even, x will end
   // up at the at the end of the first half of the array)
   const n = arr.length,
     m = Math.ceil(n/2) - 1;
   return rotate(x - m, arr);
-}
+});
--- cyclejs/lib/takeCenter.js
+++ cyclejs/lib/takeCenter.js
@@ -1,9 +1,9 @@
-import {clamp} from 'ramda';
+import {curry, clamp} from 'ramda';
 
 // left-biased: if it's ever ambigous what the 'center' element/region
 // is, the left-most of the two possible candidates is used
-export default function takeCenter(n, arr) {
+export default curry(function takeCenter(n, arr) {
   n = clamp(0, arr.length, n);
   const i = Math.floor((arr.length - n)/2);
   return arr.slice(i, i + n);
-}
+});
