6717d47d06bf83e514e5513a4d6faa3fff298feb
George Pittarelli <george.pittarelli@massdrop.com>
2016-08-28 23:31:28 -0700

    elm: Get a basic form working

--- frontends/elm/src/Main.elm
+++ frontends/elm/src/Main.elm
@@ -1,38 +1,67 @@
 import String exposing (toInt)
-import Html exposing (Html, button, input, div, form, text)
+import Html exposing (Html, button, input, div, form, text, label, br)
 import Html.App as App
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (type')
 
+
+type alias State =
+  { name : String
+  , group : Result String Int
+  , turnTime : Result String Int
+  }
+
+
+initialState : State
+initialState =
+  { name = ""
+  , group = Ok 0
+  , turnTime = Ok 0 }
+
+
+type Msg = SetName String | SetGroup String | SetTurnTime String
+
+
 main: Program Never
 main =
-    App.beginnerProgram { model = 0, view = view, update = update }
+    App.beginnerProgram { model = initialState, view = view, update = update }
 
 
-type Msg = Increment | Decrement | SetValue String
-
-
-update : Msg -> Int -> Int
+update : Msg -> State -> State
 update msg model =
   case msg of
-    Increment ->
-      model + 1
+    SetName name ->
+      { model | name = name }
 
-    Decrement ->
-      model - 1
+    SetGroup group ->
+      { model | group = toInt group }
 
-    SetValue s ->
-      case toInt s of
-        Ok x ->
-          x
-
-        Err _ ->
-          0
+    SetTurnTime turnTime ->
+      { model | turnTime = toInt turnTime }
 
 
-view : a -> Html Msg
-view model =
+view : State -> Html Msg
+view {name, group, turnTime} =
   form []
-    [ input [ type' "text", onInput SetValue ] [ text "-" ],
-      text <| toString model
+    [ label []
+        [ text "Name"
+        , input [ type' "text", onInput SetName ] []
+        ]
+    , text name
+    , br [] []
+    , label []
+        [ text "Group Id"
+        , input [ type' "number", onInput SetGroup ] []
+        ]
+    , text (case group of
+               Ok x -> toString x
+               Err e -> e)
+    , br [] []
+    , label []
+        [ text "Turn Time"
+        , input [ type' "number", onInput SetTurnTime ] []
+        ]
+    , text (case turnTime of
+               Ok x -> toString x
+               Err e -> e)
     ]
